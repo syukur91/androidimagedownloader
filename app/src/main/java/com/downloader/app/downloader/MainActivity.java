f41ec69f9ed89c2e30380940cde4166aca135f6a
syukur91 <syukur.jtk09@gmail.com>
2017-07-19 21:33:19 +0700

    optimize import

--- app/src/main/java/com/downloader/app/downloader/MainActivity.java
+++ app/src/main/java/com/downloader/app/downloader/MainActivity.java
@@ -1,45 +1,12 @@
 package com.downloader.app.downloader;
 
-import android.app.DownloadManager;
-import android.app.job.JobInfo;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Arrays;
-import java.util.Date;
-
-import android.util.Log;
-
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
-
-import static android.R.attr.name;
-
-
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-import android.view.View;
-import android.view.View.OnClickListener;
 
 
 public class MainActivity extends AppCompatActivity {
